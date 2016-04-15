069546a59d52bd3db8c5dbcf66ab511f0fe0383d
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-04-15 10:14:15 -0300

    checkout

--- resources/views/customer/order/create.blade.php
+++ resources/views/customer/order/create.blade.php
@@ -7,7 +7,7 @@
         <div class="form-group">
             <label>Total:</label>
             <p id="total"></p>
-            <a href="#" class="btn btn-default">Novo Item</a>
+            <a href="#" id="btnNewItem" class="btn btn-default">Novo Item</a>
             <table class="table table-bordered">
                 <thead>
                 <tr>
@@ -33,4 +33,37 @@
         </div>
         {!! form::close() !!}
     </div>
+@endsection
+
+@section('post-script')
+    <script>
+        $('#btnNewItem').click(function () {
+            var row = $('table tbody >tr:last'),
+                    newRow = row.clone(),
+                    length = $('table tbody tr').length;
+
+            newRow.find('td').each(function () {
+                var td = $(this),
+                        input = td.find('input,select'),
+                        name = input.attr('name');
+                input.attr('name', name.replace((length - 1) + "", length + ""));
+            });
+
+            newRow.find('input').val(1);
+            newRow.insertAfter(row);
+
+        });
+
+        function calculateTotal()
+        {
+            var total = 0;
+                trLen = $('table tbody tr').length,
+                        tr = null, price, qtd;
+
+            for(var i = 0; i < trLen; i++){
+                tr =
+            }
+        }
+
+    </script>
 @endsection
